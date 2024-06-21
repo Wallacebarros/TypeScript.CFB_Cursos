6c51fefeb8bf3b20f04ae9c154048871aa343d83
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-06-21 12:31:37 -0300

    Aprendendo sobre as configurações do TS

---
+++ configuracoes/configuracoes.js
@@ -0,0 +1,9 @@
+var Test = /** @class */ (function () {
+    function Test(nome) {
+        this.nome = null;
+        this.nome = nome;
+    }
+    return Test;
+}());
+var test = new Test("nome test");
+console.log(test);
---
+++ configuracoes/configuracoes.ts
@@ -0,0 +1,10 @@
+class Test {
+    nome=null
+    constructor(nome:any){
+        this.nome = nome
+    }
+}
+
+const test = new Test("nome test")
+
+console.log(test)
