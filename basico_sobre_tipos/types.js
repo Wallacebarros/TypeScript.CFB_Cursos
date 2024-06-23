169b27677763c49ed03471bbc2081b1231f05e3c
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-06-23 13:32:53 -0300

    Type Inference

---
+++ basico_sobre_tipos/types.js
@@ -0,0 +1,5 @@
+"use strict";
+// Type inference
+const nome2 = "tipo string";
+const numero2 = 10;
+console.log(`string: ${nome2} number: ${numero2}`);
---
+++ basico_sobre_tipos/types.ts
@@ -0,0 +1,6 @@
+// Type inference
+
+const nome2:String = "tipo string"
+const numero2:Number = 10
+
+console.log(`string: ${nome2} number: ${numero2}`);
